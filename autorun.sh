811ecf3d848bb0f1c8ade60f7f0ee6eccca9c8a1
liquidthex <github.liquid-x@liquid-x.net>
2016-01-11 19:46:53 -0800

    Made it do something

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
-gitcmd="git pull --rebase"
-success=" is up to date.$"
+gitcmd="git pull"
+success="Already up-to-date.$"
 verbose=0 # Set to 1 if you want to see output
  
 # We can't test directly with pipes, so save an output file
@@ -18,6 +18,8 @@ rm "${tmpfile}"
 if [ ! -z "${test}" ]; then   # If there is NOT new code to update...
   exit 0
 else # There IS new code to update
-  echo "Here is where you would do a kill or something."
-  exit 1  # Or just catch this exit code when you call this, and then the script calling this knows the code has been updated.
+  sudo ~/ThexBerryClock/killclock.sh
+  sleep 0.5
+  sudo ~/ThexBerryClock/ThexBerryClock.py &
+  exit 1
 fi
