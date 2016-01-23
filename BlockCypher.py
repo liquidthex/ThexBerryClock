c2c61752b3230d8fd18f200f95ac84887739cb04
liquidthex <github.liquid-x@liquid-x.net>
2016-01-23 01:08:33 -0800

    Added basic error correction

--- BlockCypher.py
+++ BlockCypher.py
@@ -12,6 +12,7 @@ webHookUrl = "http://theurgist.org/blockcypher.php"
 
 import requests
 import time
+import json
 
 def getHooks():
   print "Checking for WebHook..."
@@ -19,14 +20,28 @@ def getHooks():
   r = requests.get(url)
   print r.text
   print "------------------"
+  if r.text:
+    data = json.loads(r.text)
+    errors = data[0]['callback_errors']
+    if errors >= 5:
+      deleteHook(data[0]['id'])
   return r.text
 
+def deleteHook(hook):
+  print "Deleting old hook ID " + str(hook)
+  url = 'https://api.blockcypher.com/v1/btc/main/hooks/' + str(hook) + '?token=' + token
+  r = requests.delete(url)
+  print r.text
+  print "------------------"
+  addHook()
+  
 def addHook():
   print "Adding WebHook..."
   url = 'https://api.blockcypher.com/v1/btc/main/hooks?token=' + token
   r = requests.post(url, data = '{"event":"new-block","url":"'+webHookUrl+'"}')
   print r.text
   print "------------------"
+  getHooks()
 
 def tryHookup():
   hooks = getHooks()
